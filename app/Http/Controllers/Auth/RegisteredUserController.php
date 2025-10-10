f8e5540b827c5b16fce1887991d92cba03588247
Esteban Vargas <juanes01vargas@gmail.com>
2025-10-10 13:24:57 -0500

    Redirigir al login después del registro

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -43,8 +43,8 @@ class RegisteredUserController extends Controller
 
         event(new Registered($user));
 
-        Auth::login($user);
+        
 
-        return redirect(route('dashboard', absolute: false));
+        return redirect()->route('login')->with('success', 'Registro exitoso. Por favor, inicia sesión.');
     }
 }
