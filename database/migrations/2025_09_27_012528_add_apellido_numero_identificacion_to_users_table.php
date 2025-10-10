87c12247de24abf40d441d8b769a1e5e7b483090
Esteban Vargas <juanes01vargas@gmail.com>
2025-10-10 13:07:43 -0500

    Ajustes en migraciones antes de crear rama personal

--- database/migrations/2025_09_27_012528_add_apellido_numero_identificacion_to_users_table.php
+++ database/migrations/2025_09_27_012528_add_apellido_numero_identificacion_to_users_table.php
@@ -17,8 +17,8 @@ return new class extends Migration
 public function down(): void
 {
     Schema::table('users', function (Blueprint $table) {
-        $table->dropColumn('apellido');
-        $table->dropColumn('numero_identificacion');
+        $table->dropColumn('apellido')->nullable();
+        $table->dropColumn('numero_identificacion')->nullable();
     });
 }
 
